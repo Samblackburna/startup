a73111da0b27b72d0c3be399db6963ea797248e6
Sam Blackburn <47836435+Samblackburna@users.noreply.github.com>
2025-03-06 16:57:17 -0700

    my header/footer

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,34 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className="body bg-dark text-light">App will display here</div>;
+  return (
+    <div className="body bg-white text-dark">
+      <header>
+        <nav>
+        <img className="logo" alt="Quilted News" src="/QuiltedLogo.png" />
+          <ul>
+            <li className="nav-item">
+              <a className="nav-link active" href="index.html">Sign In</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="article.html">Articles</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="newsSourceHome.html">My News Source</a>
+            </li>
+          </ul>
+        </nav>
+        <hr style={{ marginBottom: "2px" }} />
+        <hr style={{ marginTop: "0", paddingTop: "0" }} />
+      </header>
+
+      <main>App components go here</main>
+      
+      <footer>
+        <hr />
+        <span className="text-reset">Samuel Blackburn</span>
+        <a href="https://github.com/Samblackburna/startup">GitHub</a>
+      </footer>
+    </div>
+  );
 }
