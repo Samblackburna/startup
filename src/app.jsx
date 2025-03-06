b9e445ced9154a147347cd0939c10b04bf412572
Sam Blackburn <47836435+Samblackburna@users.noreply.github.com>
2025-03-06 16:23:15 -0700

    basic app.jsx functionality

--- src/app.jsx
+++ src/app.jsx
@@ -3,34 +3,5 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return (
-    <div className="body bg-light text-dark">
-      <header>
-        <nav>
-          <img className="logo" alt="Quilted News" src="/public/QuiltedLogo.png" />
-          <ul>
-            <li className="nav-item">
-              <a className="nav-link active" href="index.html">Sign In</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="article.html">Articles</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="newsSourceHome.html">My News Source</a>
-            </li>
-          </ul>
-        </nav>
-        <hr style="margin-bottom: 2px;"/>
-        <hr style="margin-top: 0; padding-top: 0;"/>
-      </header>
-      <main>
-
-      </main>
-      <footer>
-        <hr />
-        <span class="text-reset">Samuel Blackburn</span>
-        <a href="https://github.com/Samblackburna/startup">GitHub</a>
-      </footer>
-    </div>
-  );
+  return <div className="body bg-dark text-light">App will display here</div>;
 }
