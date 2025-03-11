a9ad2425c2323010a93a511397cab4c5a2f5e974
Sam Blackburn <47836435+Samblackburna@users.noreply.github.com>
2025-03-11 01:03:48 -0600

    big milestone

--- src/app.jsx
+++ src/app.jsx
@@ -2,35 +2,51 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Articles } from './articles/articles';
+import { NewsSourceHome } from './newsSourceHome/newsSourceHome';
+
 export default function App() {
   return (
-    <div className="body bg-white text-dark">
-      <header>
-        <nav>
-        <img className="logo" alt="Quilted News" src="/QuiltedLogo.png" />
-          <ul>
-            <li className="nav-item">
-              <a className="nav-link active" href="index.html">Sign In</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="article.html">Articles</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="newsSourceHome.html">My News Source</a>
-            </li>
-          </ul>
-        </nav>
-        <hr style={{ marginBottom: "2px" }} />
-        <hr style={{ marginTop: "0", paddingTop: "0" }} />
-      </header>
+    <BrowserRouter>
+      <div className="body">
+        <header>
+          <nav>
+            <img className="logo" alt="Quilted News" src="/QuiltedLogo.png" />
+            <ul>
+              <li className="nav-item">
+                <NavLink className="nav-link active" to="/">Sign In</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="articles">Articles</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="newsSourceHome">My News Source</NavLink>
+              </li>
+            </ul>
+          </nav>
+          <hr style={{ marginBottom: "2px" }} />
+          <hr style={{ marginTop: "0", paddingTop: "0" }} />
+        </header>
 
-      <main>App components go here</main>
-      
-      <footer>
-        <hr />
-        <span className="text-reset">Samuel Blackburn</span>
-        <a href="https://github.com/Samblackburna/startup">GitHub</a>
-      </footer>
-    </div>
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/articles' element={<Articles />} />
+          <Route path='/newsSourceHome' element={<NewsSourceHome />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+        
+        <footer>
+          <hr />
+          <span className="text-reset">Samuel Blackburn</span>
+          <a href="https://github.com/Samblackburna/startup">GitHub</a>
+        </footer>
+      </div>
+    </BrowserRouter>
   );
+}
+
+function NotFound() {
+  return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
 }
---
+++ src/newsSourceHome/newsSourceHome.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function NewsSourceHome() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>news source information displayed here</div>
+    </main>
+  );
+}
