cdbfb7e096b3a37e03d282e80bd2f8214ff6cc91
Sam Blackburn <47836435+Samblackburna@users.noreply.github.com>
2025-03-13 15:07:49 -0600

    working on login

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 
 export function Login() {
+  const [text, setText] = React.useState('');
+
   const handleButtonClick = (event) => {
     event.preventDefault();
+  }
+
+  function loginUser(event) {
+    event.preventDefault();
+    localStorage.setItem('user', text);
+  }
+
+  function textChange(e) {
+    setText(e.target.value);
   }
 
   return (
@@ -10,17 +21,17 @@ export function Login() {
       <div>
         <h3 className="sign-in-notice">Sign in or create account</h3>
       </div>
-      <form method="get" action="play.html">
+      <form>
         <div style={{ width: '100%' }}>
           <span className="sign-in">Email or Username</span>
-          <input type="text" />
+          <input type="text" onChange={textChange} />
         </div>
         <div style={{ width: '100%' }}>
           <span className="sign-in">Password</span>
-          <input type="text" />
+          <input type="text" onChange={textChange} />
         </div>
         <div style={{ width: '100%' }} className="button-holder">
-          <button style={{ width: '100%' }} type="submit" onClick={handleButtonClick}>Sign in</button>
+          <button style={{ width: '100%' }} type="submit" onClick={loginUser}>Login</button>
           <button type="submit" onClick={handleButtonClick}>Create</button>
         </div>
       </form>
