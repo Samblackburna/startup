363ec1d25db2d021629338b46b8bc614f074e8f5
Sam Blackburn <47836435+Samblackburna@users.noreply.github.com>
2025-03-25 15:43:54 -0600

    backend

--- service/index.js
+++ service/index.js
@@ -1,2 +1,16 @@
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
+const express = require('express');
+const uuid = require('uuid');
+const app = express();
+
+const authCookieName = 'token';
+
+// From SIMON: The service port. In production the front-end code is statically hosted by the service on the same port.
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
+
 app.use(express.static('public'));
+
+//
+app.use(express.json());
